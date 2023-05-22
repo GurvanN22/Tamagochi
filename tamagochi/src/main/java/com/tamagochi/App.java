42e07a3ed1b4a0d58c362cfe85438acc9c13d2c7
GurvanN22 <93199510+GurvanN22@users.noreply.github.com>
2023-05-22 11:03:04 +0200

    add all scenes and css

--- tamagochi/src/main/java/com/tamagochi/App.java
+++ tamagochi/src/main/java/com/tamagochi/App.java
@@ -20,8 +20,12 @@ import javafx.stage.Stage;
 public class App extends Application {
 
     private Scene mainScene;
+    private Scene feedScene;
+    private Scene playScene;
+    private Scene HealScene;
+    private Scene cleanScene;
 
-    public void initMenu(Stage stage) {
+    private void initMenu(Stage stage) {
         Button feedButton = new Button("Feed");
         feedButton.getStyleClass().add("main-menu-button");
         Button playButton = new Button("Play");
@@ -30,9 +34,25 @@ public class App extends Application {
         healButton.getStyleClass().add("main-menu-button");
         Button cleanButton = new Button("clean");
         cleanButton.getStyleClass().add("main-menu-button");
+
         feedButton.setOnAction(e -> {
-            
+            stage.setScene(feedScene);
+            stage.show();
         });
+        playButton.setOnAction(e -> {
+            stage.setScene(playScene);
+            stage.show();
+        });
+        healButton.setOnAction(e -> {
+            stage.setScene(HealScene);
+            stage.show();
+        });
+        cleanButton.setOnAction(e -> {
+            stage.setScene(cleanScene);
+            stage.show();
+        });
+
+
         HBox H1 = new HBox(feedButton , healButton);
         HBox H2 = new HBox(playButton , cleanButton);
         VBox V1 = new VBox( H1 , H2);
@@ -43,9 +63,75 @@ public class App extends Application {
         stage.show();
     }
 
+    private void initFeed(Stage stage) {
+        Button returnButton = new Button("return");
+        returnButton.getStyleClass().add("main-menu-button");
+        returnButton.setOnAction(e -> {
+            stage.setScene(mainScene);
+            stage.show();
+        });
+        VBox V1 = new VBox( returnButton);
+        feedScene = new Scene(V1, 640, 480);
+        feedScene.setOnKeyPressed(event -> {
+        String codeString = event.getCode().toString();});
+        String cssPath = getClass().getResource("/css/feed-menu.css").toString();
+        feedScene.getStylesheets().add(cssPath);
+        stage.setScene(mainScene);
+        stage.show();
+    }
+
+    private void initGame(Stage stage) {
+        Button returnButton = new Button("return");
+        returnButton.getStyleClass().add("main-menu-button");
+        returnButton.setOnAction(e -> {
+            stage.setScene(mainScene);
+            stage.show();
+        });
+        VBox V1 = new VBox( returnButton);
+        playScene = new Scene(V1, 640, 480);
+
+        String cssPath = getClass().getResource("/css/play-menu.css").toString();
+        playScene.getStylesheets().add(cssPath);
+        stage.setScene(mainScene);
+        stage.show();
+    }
+
+    private void initHeal(Stage stage) {
+        Button returnButton = new Button("return");
+        returnButton.getStyleClass().add("main-menu-button");
+        returnButton.setOnAction(e -> {
+            stage.setScene(mainScene);
+            stage.show();
+        });
+        VBox V1 = new VBox( returnButton);
+        HealScene = new Scene(V1, 640, 480);
+        String cssPath = getClass().getResource("/css/heal-menu.css").toString();
+        HealScene.getStylesheets().add(cssPath);
+        stage.setScene(mainScene);
+        stage.show();
+    }
+    private void initClean(Stage stage) {
+        Button returnButton = new Button("return");
+        returnButton.getStyleClass().add("main-menu-button");
+        returnButton.setOnAction(e -> {
+            stage.setScene(mainScene);
+            stage.show();
+        });
+        VBox V1 = new VBox( returnButton);
+        cleanScene = new Scene(V1, 640, 480);
+        String cssPath = getClass().getResource("/css/clean-menu.css").toString();
+        cleanScene.getStylesheets().add(cssPath);
+        stage.setScene(mainScene);
+        stage.show();
+    }
+
     @Override
     public void start(Stage stage) {
         initMenu(stage);
+        initFeed(stage);
+        initGame(stage);
+        initHeal(stage);
+        initClean(stage);
     }
 
     public static void main(String[] args) {
