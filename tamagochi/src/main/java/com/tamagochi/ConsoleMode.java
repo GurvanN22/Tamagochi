6db918fb33665bb198ab2ca3742a0c5c8bcc61f4
GurvanN22 <93199510+GurvanN22@users.noreply.github.com>
2023-05-31 11:32:00 +0200

    update

--- tamagochi/src/main/java/com/tamagochi/ConsoleMode.java
+++ tamagochi/src/main/java/com/tamagochi/ConsoleMode.java
@@ -1,19 +1,53 @@
 package com.tamagochi;
 
+import java.nio.file.Paths;
+
+import com.Tamagochi;
+
+import javafx.scene.shape.Path;
+
 public class ConsoleMode {
-    public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_BLACK = "\u001B[30m";
-    public static final String ANSI_RED = "\u001B[31m";
-    public static final String ANSI_GREEN = "\u001B[32m";
-    public static final String ANSI_YELLOW = "\u001B[33m";
-    public static final String ANSI_BLUE = "\u001B[34m";
-    public static final String ANSI_PURPLE = "\u001B[35m";
-    public static final String ANSI_CYAN = "\u001B[36m";
-    public static final String ANSI_WHITE = "\u001B[37m";
-    public static final String PURPLE = "\033[0;35m";  // PURPLE
-    public static final String WHITE_BACKGROUND = "\033[47m";
+    public static final String reset = "\u001B[0m";
+    public static final String purple = "\033[0;35m";
+    public static final String whiteBack = "\033[47m";
+    public static final String purpleBack = "\u001B[45m";
+    public static final String redBack = "\u001B[41m";
+    public static final String yellowBack = "\u001B[43m";
+    Tamagochi pet = new Tamagochi("Bird");
     ConsoleMode() {
         System.out.print("\033[H\033[2J");  
-        System.out.println(WHITE_BACKGROUND + WHITE_BACKGROUND +  "Welcome to the tamagochi console mode !");
+        System.out.println("Welcome to the tamagochi console mode !\n\n\n\n\n\n\n");
+        try {
+            String asciiBeast = getClass().getResource("/AsciiArt/bird").toString();
+            System.out.println(" Ton tamagochi ? (temp)");
+        }
+        catch( Exception e) {
+            System.out.println("Error while loading the ascii art");
+        }
+        displayPetInfos();
+    }
+    private void displayPetInfos() {
+        System.out.println("Name : " + pet.name);
+        System.out.println("Age : " + pet.age);
+        System.out.println("Health : ");
+        displayColorLine(redBack, pet.health );
+        System.out.println("Hunger : ");
+        displayColorLine(yellowBack, pet.hunger);
+        System.out.println("Happiness : ");
+        displayColorLine(purpleBack, pet.happiness*2);
+    }
+    void displayColorLine(String color,int Number) {
+        String coloredSpaces = "";
+        String whiteSpaces = "";
+        while (Number > 0) {
+            coloredSpaces += " ";
+            Number--;
+        }
+        Number = 100 - coloredSpaces.length();
+        while (Number > 0) {
+            whiteSpaces += " ";
+            Number--;
+        }
+        System.out.println(color + coloredSpaces + whiteBack + whiteSpaces + reset);
     }
 }
