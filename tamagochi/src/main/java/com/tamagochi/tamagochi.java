0519089b795b8f2bedc048078002ba0d9ddf8425
noa <nxa.drxuineau@gmail.com>
2023-06-02 08:48:10 +0200

    add timer

--- tamagochi/src/main/java/com/tamagochi/tamagochi.java
+++ tamagochi/src/main/java/com/tamagochi/tamagochi.java
@@ -1,11 +1,9 @@
 package com.tamagochi;
 
-public class Tamagotchi implements Runnable {
-        static int happiness = 15;
-        static int timeUnitPassed = 0;
-        
-        @Override
-        public void run() {
-                Thread timer = new Thread(new Tamagochi());
-        }
+public class Tamagochi {
+
+    public static void main(String[] args) {
+        Thread timer = new Thread(new MinuteTimer());
+        timer.start();
     }
+}
