39c2e01bbd3bf6222e588235dd4e5d641f611167
GurvanN22 <93199510+GurvanN22@users.noreply.github.com>
2023-06-11 00:51:31 +0200

    add functional fx tamagochi

--- tamagochi/src/main/java/com/tamagochi/FXmode.java
+++ tamagochi/src/main/java/com/tamagochi/FXmode.java
@@ -1,107 +1,153 @@
 package com.tamagochi;
 
-import javafx.application.Application;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-public class FXmode extends Application implements Runnable {
+public class FXmode implements Save{
+    private Timer timer;
     /**
-     * The main scene of the tamagotchi.
-     **/
+    * The main scene of the tamagochi.
+    **/
     private Scene mainScene;
+    Stage stage;
+    Tamagochi pet;
     /**
-     *  The feed scene of the tamagotchi
-     **/
-    private Scene feedScene;
-    /**
-     *  The play scene of the tamagotchi
-     **/
-    private Scene playScene;
-    /**
-     *  The heal scene of the tamagotchi
-     **/
-    private Scene healScene;
-    /**
-     *  The clean scene of the tamagotchi
-     **/
-    private Scene cleanScene;
-
-    private Stage stage;
-
-    // public FXmode(Stage stage) {
-    // }
-
-    public static void main(String[] args) {
-        FXmode fxMode = new FXmode();
-        Thread thread = new Thread(fxMode);
-        thread.start();
-    }
-
-    @Override
-    public void run() {
-        launch(new String[]{});
-    }
-
-    @Override
-    public void start(Stage stage) {
+    * start the tamagochi
+    **/
+    FXmode(Stage stage) {
         this.stage = stage;
+        pet = load();
+        if (pet.IsNew == true) {
+            pet.IsNew = false;
+            pet.name = "tama";
+        }
         initMenu();
-        initFeed();
-        initGame();
-        initHeal();
-        initClean();
+        timer = new Timer();
+        timer.schedule(new GrowTask(), 0, 10000);
     }
-
     /**
-     * init the main menu of the tamagotchi
-     **/
+    * init the main menu of the tamagochi
+    **/
     private void initMenu() {
-        // ... le code du menu principal
-
-        stage.setScene(mainScene);
-        stage.show();
+        if(pet.IsDead == true) {
+            Label deadLabel = new Label("tama pet is dead , RIP");
+            deadLabel.getStyleClass().add("main-menu-text");
+            Button exitButton = new Button("exit");
+            exitButton.setOnAction(e -> {
+                save(pet);
+                System.exit(0);
+            });
+            VBox V2 = new VBox(deadLabel , exitButton);
+            mainScene = new Scene(V2, 640, 480);
+            String cssPath = getClass().getResource("/css/main-menu.css").toString();
+            mainScene.getStylesheets().add(cssPath);
+            stage.setScene(mainScene);
+            stage.show();
+        } else {
+            if (pet.IsEgg == true) {
+                Label EggLabel = new Label("tama is a egg wait 1 day");
+                EggLabel.getStyleClass().add("main-menu-text");
+                mainScene = new Scene(EggLabel, 640, 480);
+                String cssPath = getClass().getResource("/css/main-menu.css").toString();
+                mainScene.getStylesheets().add(cssPath);
+                stage.setScene(mainScene);
+                stage.show();
+            } else {
+                Label feedLabel;        
+                Label cleanLabel;           
+                Label sickLabel;
+                
+                
+                
+                if(pet.foodTime == true) {
+                    feedLabel = new Label(pet.name + " is not hungry anymore");
+                } else {
+                    feedLabel = new Label(pet.name + " is hungry");
+                }
+                if(pet.Isclean == true) {
+                    cleanLabel = new Label(pet.name + " is clean");
+                } else {
+                    cleanLabel = new Label(pet.name + " is dirty");
+                }
+                if(pet.IsSick == true) {
+                    sickLabel = new Label(pet.name + " is Sick");
+                } else {
+                    sickLabel = new Label(pet.name + " is Healthy");
+                }
+                Label hapiness = new Label("Happiness" + pet.happiness + "/50");
+                Label name = new Label("Name : " + pet.name);
+                Label age = new Label("Age " + pet.age + " days");
+                feedLabel.getStyleClass().add("main-menu-text");
+                cleanLabel.getStyleClass().add("main-menu-text");
+                sickLabel.getStyleClass().add("main-menu-text");
+                hapiness.getStyleClass().add("main-menu-text");
+                name.getStyleClass().add("main-menu-text");
+                age.getStyleClass().add("main-menu-text");
+                
+                Button feedButton = new Button("Feed");
+                feedButton.getStyleClass().add("main-menu-button-feed");
+                Button playButton = new Button("Play");
+                playButton.getStyleClass().add("main-menu-button-play");
+                Button healButton = new Button("heal");
+                healButton.getStyleClass().add("main-menu-button-heal");
+                Button cleanButton = new Button("clean");
+                cleanButton.getStyleClass().add("main-menu-button-clean");
+                
+                feedButton.setOnAction(e -> {
+                    pet.feed();
+                    initMenu();
+                });
+                playButton.setOnAction(e -> {
+                    pet.play();
+                    initMenu();
+                });
+                healButton.setOnAction(e -> {
+                    pet.heal();
+                    initMenu();
+                });
+                cleanButton.setOnAction(e -> {
+                    pet.wash();
+                    initMenu();
+                });
+                
+                HBox H1 = new HBox(feedButton , healButton);
+                HBox H2 = new HBox(playButton , cleanButton);        
+                VBox V2 = new VBox(name , age , hapiness , feedLabel , cleanLabel , sickLabel);
+                VBox V1 = new VBox(V2 ,H1 , H2);
+                mainScene = new Scene(V1, 840, 680);
+                String cssPath = getClass().getResource("/css/main-menu.css").toString();
+                mainScene.getStylesheets().add(cssPath);
+                stage.setScene(mainScene);
+                stage.show();
+            }
+            
+        }
+        
     }
-
-    /**
-     * init the feed menu of the tamagotchi
-     **/
-    private void initFeed() {
-        // ... le code du menu de nourriture
-
-        stage.setScene(mainScene);
-        stage.show();
+    private void grow() {
+        Platform.runLater(() -> {
+            pet.grow();
+            initMenu();
+            System.out.println(pet.happiness);
+            save(pet);
+        });
     }
-
-    /**
-     * init the game menu of the tamagotchi
-     **/
-    private void initGame() {
-        // ... le code du menu de jeu
-
-        stage.setScene(mainScene);
-        stage.show();
-    }
-
-    /**
-     * init the heal menu of the tamagotchi
-     **/
-    private void initHeal() {
-        // ... le code du menu de guérison
-
-        stage.setScene(mainScene);
-        stage.show();
-    }
-
-    /**
-     * init the clean menu of the tamagotchi
-     **/
-    private void initClean() {
-        // ... le code du menu de nettoyage
-
-        stage.setScene(mainScene);
-        stage.show();
+    private class GrowTask extends TimerTask {
+        @Override
+        public void run() {
+            ExecutorService executor = Executors.newSingleThreadExecutor();
+            executor.execute(FXmode.this::grow);
+            executor.shutdown();
+        }
     }
 }
